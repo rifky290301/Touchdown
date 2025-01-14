84b77b2dccfcfd3689774ed2a64c9a3b214c10b4
rifky290301 <rifky290301@gmail.com>
2025-01-14 10:22:08 +0700

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by MarthaBakManis on 14/01/25.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
